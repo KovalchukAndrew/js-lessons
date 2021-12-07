8100470e9d07fcdcfbb22f402639ae4939d802cc
KovalchukAndrew <dry.st.jim@gmail.com>
2021-12-07 17:42:31 +0300

    search-films

--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -2,21 +2,31 @@ import React, {useState} from 'react';
 import API from './API';
 import './lesson_3';
 
+type Response = Array<FilmType>
+
+type FilmType = {
+    Title: string,
+    Year: string,
+    imdbID: string,
+    Type: string,
+    Poster: string
+}
+
 const Lesson3 = () => {
     const [searchName, setSearchName] = useState('');
-    const [serachResult, setSerachResult] = useState('');
+    const [serachResult, setSerachResult] = useState<Response>([]);
     const [searchNameByType, setSearchNameByType] = useState('');
     const [serachResultByType, setSerachResultByType] = useState('');
+
 
     const searchFilm = () => {
         API.searchFilmsByTitle(searchName)
             .then(response => {
-               // response.data.Response === "True" ?
-                    //setSerachResult(response.data.Search.Title) :
+               //response.data.Response === "True" ?
+                    setSerachResult(response.data.Search)
                     //setSerachResult("Error")
-                //setSerachResult(response.data.Search)
 
-                //console.log(response.data)
+                                //console.log(response.data)
 
             })
             .catch(err => {
@@ -40,7 +50,8 @@ const Lesson3 = () => {
                 <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>
                 <button onClick={searchFilm}>Search</button>
                 <div>
-                    {serachResult}
+                    {serachResult.map(el => <img src={el.Poster} alt=""/> )}
+
                 </div>
             </div>
 
